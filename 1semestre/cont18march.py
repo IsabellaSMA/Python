ae0cf3325caea7268e5a3ac705e67caaad694917
Isabella Alves <86487337+IsabellaSMA@users.noreply.github.com>
2024-05-26 21:29:32 +0000

    updates

--- 1semestre/cont18march.py
+++ 1semestre/cont18march.py
@@ -5,7 +5,9 @@ num2 = int(input('Digite um número: '))
 soma = num1+num2
 if soma % 2 == 0:
     print(
-        f'A soma dos números {num1} e {num2} é igual á: {soma}.Resultando em PAR')
+        f'A soma dos números {num1} e {num2} é igual á: {soma}.Resultando em PAR'
+    )
 else:
     print(
-        f'A soma dos números {num1} e {num2} é igual á: {soma}.Resultando em IMPAR')
+        f'A soma dos números {num1} e {num2} é igual á: {soma}.Resultando em IMPAR'
+    )
